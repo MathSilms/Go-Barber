3bb85adec3f1b3c645e4316269605c29361f8ea0
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-07-19 01:25:54 -0300

    Validation

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '851b1d7f2b9d29403d27a6ccaffd6dda',
+  expiresIn: '7d',
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,18 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+// import Controllers
 import UserController from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionController';
+
+// import Middlewares
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 // Users
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionControler.store);
+routes.put('/users', authMiddleware, SessionControler.update);
 
 export default routes;
