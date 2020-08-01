ffd507c3df47d31c0af7268de13791383f29b71f
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2020-08-01 16:27:09 -0300

    configuring sentry end express-erros

--- backend/src/app/controllers/AvaliableController.js
+++ backend/src/app/controllers/AvaliableController.js
@@ -1,4 +1,12 @@
-import { startOfDay, endOfDay } from 'date-fns';
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
 import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
 
@@ -12,7 +20,7 @@ class AvaliableController {
 
     const searchDate = Number(date);
 
-    const Appointments = await Appointment.findAll({
+    const appointments = await Appointment.findAll({
       where: {
         provider_id: req.params.providerId,
         canceled_at: null,
@@ -22,9 +30,38 @@ class AvaliableController {
       },
     });
 
-    console.log(Appointments);
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
 
-    return res.json(Appointments);
+    const avaliable = schedule.map((time) => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyy-MM-dd'T'HH:mm:ssxxx"),
+        avaliable:
+          isAfter(value, new Date()) &&
+          !appointments.find((a) => format(a.date, 'HH:mm') === time),
+      };
+    });
+
+    return res.json(avaliable);
   }
 }
 
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn:
+    'https://03952caeef89495a91358d607d3221bb@o428675.ingest.sentry.io/5374318',
+};
